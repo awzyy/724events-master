450bba0cb02d5c25d9afb85c5d2a5783e71a14eb
awzyy <waysbdo@gmail.com>
2023-10-17 10:37:09 +0200

    fliters fix

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -14,10 +14,7 @@ const EventList = () => {
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
   const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
+    (!type ? data?.events : data?.events.filter((event) => event.type === type)) || []).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
       PER_PAGE * currentPage > index
@@ -27,8 +24,9 @@ const EventList = () => {
     return false;
   });
   const changeType = (evtType) => {
-    setCurrentPage(1);
-    setType(evtType);
+    if (evtType !== type)
+    setCurrentPage(1); 
+    setType(evtType); 
   };
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
