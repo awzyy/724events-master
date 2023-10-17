db1815247e46a18b3afc6744d4c76e05b0a4b8b2
awzyy <waysbdo@gmail.com>
2023-10-17 10:57:07 +0200

    events sorting by date

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -28,6 +28,7 @@ const EventList = () => {
     setCurrentPage(1); 
     setType(evtType); 
   };
+  filteredEvents.sort((eventA, eventB) => new Date(eventA.date) - new Date(eventB.date))
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
