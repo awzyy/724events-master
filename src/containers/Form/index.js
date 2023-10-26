215a7bb5e4b171a79f3aad1514ad6db9843006f8
awzyy <waysbdo@gmail.com>
2023-10-26 16:02:34 +0200

    fix form

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,19 +4,24 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+
+const mockContactApi = () =>
+  new Promise((resolve) => {
+    setTimeout(resolve, 200);
+  });
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
+
   const sendContact = useCallback(
     async (evt) => {
       evt.preventDefault();
-      setSending(true);
       // We try to call mockContactApi
       try {
+        setSending(true);
         await mockContactApi();
+        onSuccess();
         setSending(false);
-        onSuccess()
       } catch (err) {
         setSending(false);
         onError(err);
@@ -38,7 +43,8 @@ const Form = ({ onSuccess, onError }) => {
             titleEmpty
           />
           <Field placeholder="" label="Email" />
-          <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
+          <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}
+          >
             {sending ? "En cours" : "Envoyer"}
           </Button>
         </div>
@@ -57,11 +63,11 @@ const Form = ({ onSuccess, onError }) => {
 Form.propTypes = {
   onError: PropTypes.func,
   onSuccess: PropTypes.func,
-}
+};
 
 Form.defaultProps = {
   onError: () => null,
   onSuccess: () => null,
-}
+};
 
 export default Form;
